7696a161903dd1804b84a3b8a52f0a020fc27b79
yasin abbasspour <yasin.absr83@gmail.com>
2025-05-30 12:26:40 +0330

    add dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import BlogDashboard from '@/components/dashboard/blog-dashboard'
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div><BlogDashboard/></div>
+  )
+}
+
+export default Dashboard
---
+++ components/dashboard/blog-dashboard.tsx
@@ -0,0 +1,68 @@
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
+import { ChartBar, FileText, MessageCircle, PlusCircle } from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+
+const BlogDashboard = () => {
+  return (
+    <main className="flex-1 p-4 md:p-8">
+      <div className="flex justify-between items-center mb-8">
+        <div>
+          <h1 className="font-bold text-2xl">Blog Dashboard</h1>
+          <p>Manage your content and analytics</p>
+        </div>
+
+        <Link href={"/dashboard/articles/create"}>
+          <Button>
+            <PlusCircle className="h-4 w-4" />
+            New Article
+          </Button>
+        </Link>
+      </div>
+
+      <div className="grid md:grid-cols-3 mb-8 gap-4">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="font-medium text-sm">
+              Total Comments
+            </CardTitle>
+            <MessageCircle className="h-5 w-5" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">2</div>
+            <p className="text-sm text-muted-foreground mt-1 ">13 awaiting moderation</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="font-medium text-sm">
+              Avg Raiting Time
+            </CardTitle>
+            <ChartBar className="h-5 w-5" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">4.3</div>
+            <p className="text-sm text-muted-foreground mt-1 ">+0.5 from last month</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="font-medium text-sm">
+              Total Articles
+            </CardTitle>
+            <FileText className="h-5 w-5" />
+          </CardHeader >
+          <CardContent>
+            <div className="text-2xl font-bold">2</div>
+            <p className="text-sm text-muted-foreground mt-1 ">+ 5 from last month</p>
+          </CardContent>
+        </Card>
+      </div>
+    </main>
+  );
+};
+
+export default BlogDashboard;
