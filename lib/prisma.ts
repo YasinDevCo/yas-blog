a584e650946a02f5f986895c46f9f07b5f7ab44c
yasin abbasspour <yasin.absr83@gmail.com>
2025-06-05 17:03:16 +0330

    add6

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-declare global {
-  // به جای "let" از "var" استفاده کن
-  // و به نوع "NodeJS.Global" گسترش بده
-  // تا تایپ اسکریپت بفهمه global.prisma داریم
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
-export const prisma = global.prisma ?? new PrismaClient();
+const prisma = new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+export { prisma };
